98b5fc7d4f25d85f471ab8437769d36e788fc4ad
Myles Young <myles.young@mailchimp.com>
2020-11-07 01:19:11 -0500

    abstract layout for most sections

---
+++ src/Main/Showcase/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import { Container } from '../../BsComponents';
+import { Grid, Center } from '../../Components';
+
+const ShowCase = ({
+  id = '',
+  children,
+  description,
+  title = 'Title',
+  shouldCenter = true,
+}) => (
+  <Center id={id} className="text-center" totallyCentered>
+    <div className=" section-title">
+      <h2>{title}</h2>
+      {description ? <p>{description}</p> : null}
+    </div>
+    {shouldCenter ? (
+      <Center totallyCentered>
+        <Grid>{children || 'loading...'}</Grid>
+      </Center>
+    ) : (
+      <Grid>{children || 'loading...'}</Grid>
+    )}
+  </Center>
+);
+
+// const ShowCase = ({ id = '', children, title = 'Title' }) => (
+//   <div id={id} className="text-center">
+//     <Container hasRow={false}>
+//       <div className="col-md-10 col-md-offset-1 section-title">
+//         <h2>{title}</h2>
+//       </div>
+//       <Container showRowOnly>{children || 'loading...'}</Container>
+//     </Container>
+//   </div>
+// );
+
+ShowCase.propTypes = {
+  children: PropTypes.node,
+  description: PropTypes.string,
+  id: PropTypes.string,
+  shouldCenter: PropTypes.bool,
+  title: PropTypes.string,
+};
+
+export default ShowCase;
