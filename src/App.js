63483d7b3e1e6ad2728f154672ba244c88d2e6b0
Myles Young <myles.young@mailchimp.com>
2020-11-06 19:30:30 -0500

    folder restructure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Features from './components/Features';
+import About from './components/About';
+import Services from './components/Services';
+import Gallery from './components/Gallery';
+import Testimonials from './components/Testimonials';
+import Team from './components/Team';
+import Contact from './components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {initialData} from './data';
+
+export default function App() {
+  const [landingPageData, setLandingPageData] = useState(null);
+
+  useEffect(() => {
+    setLandingPageData(initialData);
+  }, [landingPageData]);
+
+    
+    return (
+      <div>
+        <Navbar />
+        {landingPageData && (
+          <>
+          
+          <Header data={landingPageData.Header} />
+          <Features data={landingPageData.Features} />
+          <About data={landingPageData.About} />
+          <Services data={landingPageData.Services} />
+          <Gallery />
+          <Testimonials data={landingPageData.Testimonials} />
+          <Team data={landingPageData.Team} />
+          <Contact data={landingPageData.Contact} />
+          </>
+
+        )}
+      </div>
+    )
 }
 
-export default App;
