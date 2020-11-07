d2ee7dbf1df0232819eb855b1623e38ddcdf8cd7
Myles Young <myles.young@mailchimp.com>
2020-11-07 01:23:10 -0500

    rebuild header using my own components

--- src/Main/Header/index.js
+++ src/Main/Header/index.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Container } from '../../BsComponents';
+import { Center } from '../../Components';
+
 const Header = ({ data = {} }) => (
   <header id="header">
     <div className="intro">
       <div className="overlay">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 intro-text">
-              <h1>
-                {data ? data.title : 'Loading'}
-                <span />
-              </h1>
-              <p>{data ? data.paragraph : 'Loading'}</p>
-              <a href="#features" className="btn btn-custom btn-lg page-scroll">
-                Learn More
-              </a>
-            </div>
-          </div>
-        </div>
+        <Container>
+          <Center className="intro-text">
+            <h1>{data ? data.title : 'Loading'}</h1>
+            <p>{data ? data.paragraph : 'Loading'}</p>
+            <a href="#features" className="btn btn-custom btn-lg page-scroll">
+              Learn More
+            </a>
+          </Center>
+        </Container>
       </div>
     </div>
   </header>
 );
+// const Header = ({ data = {} }) => (
+//   <header id="header">
+//     <div className="intro">
+//       <div className="overlay">
+//         <Container>
+//           {/* type, size, classname */}
+//           <div className="col-md-8 col-md-offset-2 intro-text">
+//             <h1>
+//               {data ? data.title : 'Loading'}
+//               <span />
+//             </h1>
+//             <p>{data ? data.paragraph : 'Loading'}</p>
+//             <a href="#features" className="btn btn-custom btn-lg page-scroll">
+//               Learn More
+//             </a>
+//           </div>
+//         </Container>
+//       </div>
+//     </div>
+//   </header>
+// );
 
 Header.propTypes = {
   data: PropTypes.object,
