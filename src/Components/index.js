e6102ddc24cc13b842f2e053f4eed70204a78b95
Myles Young <myles.young@mailchimp.com>
2020-11-07 08:43:32 -0500

    remove unneeded

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,9 +3,7 @@ import Button from './Button';
 import Card from './Card';
 import Center from './Center';
 import Divider from './Divider';
-import Footer from './Footer';
 import Grid from './Grid';
-import Page from './Page';
 import StackLayout from './StackLayout';
 
-export { Box, Button, Card, Center, Divider, Footer, Grid, Page, StackLayout };
+export { Box, Button, Card, Center, Divider, Grid, StackLayout };
