34f7fb687063822f690449079da7985a56edc694
Myles Young <myles.young@mailchimp.com>
2020-11-07 01:22:40 -0500

    small refactor to contact section

--- src/Main/Contact/index.js
+++ src/Main/Contact/index.js
@@ -1,130 +1,125 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Contact({ data = {} }) {
-  return (
-    <div>
-      <div id="contact">
-        <div className="container">
-          <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
-              </div>
-              <form name="sentMessage" id="contactForm" noValidate>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required="required"
-                      />
-                      <p className="help-block text-danger" />
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required="required"
-                      />
-                      <p className="help-block text-danger" />
-                    </div>
-                  </div>
-                </div>
+import { Container, SocialLinks } from '../../BsComponents';
+
+const ContactItemContainer = ({ children, heading = '' }) => (
+  <div className="contact-item">
+    <h3>{heading || null}</h3>
+    {children}
+  </div>
+);
+
+ContactItemContainer.propTypes = {
+  children: PropTypes.node,
+  heading: PropTypes.string,
+};
+
+const ContactItem = ({ data = {}, itemType = '', icon }) => (
+  <p>
+    <span>
+      {icon || null} {itemType}
+    </span>
+    {data[itemType.toLowerCase()] ?? 'loading'}
+  </p>
+);
+
+ContactItem.propTypes = {
+  data: PropTypes.object,
+  icon: PropTypes.node,
+  itemType: PropTypes.string,
+};
+
+const Contact = ({ data = {} }) => (
+  <div id="contact">
+    <Container hasRow={false}>
+      <div className="col-md-8">
+        <Container showRowOnly>
+          <div className="section-title">
+            <h2>Get In Touch</h2>
+            <p>
+              Please fill out the form below to send us an email and we will get
+              back to you as soon as possible.
+            </p>
+          </div>
+          <form name="sentMessage" id="contactForm" noValidate>
+            <Container showRowOnly>
+              <div className="col-md-6">
                 <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
+                  <input
+                    type="text"
+                    id="name"
                     className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
+                    placeholder="Name"
+                    required="required"
                   />
                   <p className="help-block text-danger" />
                 </div>
-                <div id="success" />
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker" /> Address
-                </span>
-                {data ? data.address : 'loading'}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone" /> Phone
-                </span>
-                {data ? data.phone : 'loading'}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o" /> Email
-                </span>
-                {data ? data.email : 'loading'}
-              </p>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={data ? data.facebook : '/'}>
-                      <i className="fa fa-facebook" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href={data ? data.twitter : '/'}>
-                      <i className="fa fa-twitter" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href={data ? data.youtube : '/'}>
-                      <i className="fa fa-youtube" />
-                    </a>
-                  </li>
-                </ul>
               </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    id="email"
+                    className="form-control"
+                    placeholder="Email"
+                    required="required"
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+              </div>
+            </Container>
+            <div className="form-group">
+              <textarea
+                name="message"
+                id="message"
+                className="form-control"
+                rows="4"
+                placeholder="Message"
+                required
+              />
+              <p className="help-block text-danger" />
             </div>
-          </div>
-        </div>
+            <div id="success" />
+            <button type="submit" className="btn btn-custom btn-lg">
+              Send Message
+            </button>
+          </form>
+        </Container>
       </div>
-      <div id="footer">
-        <div className="container text-center">
-          <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
-          </p>
-        </div>
+
+      <div className="col-md-3 col-md-offset-1 contact-info">
+        <ContactItemContainer heading="Contact Info">
+          <ContactItem
+            {...{
+              data,
+              itemType: 'Address',
+              icon: <i className="fa fa-map-marker" />,
+            }}
+          />
+          <ContactItem
+            {...{
+              data,
+              itemType: 'Phone',
+              icon: <i className="fa fa-phone" />,
+            }}
+          />
+          <ContactItem
+            {...{
+              data,
+              itemType: 'Email',
+              icon: <i className="fa fa-envelope-o" />,
+            }}
+          />
+        </ContactItemContainer>
       </div>
-    </div>
-  );
-}
+
+      <div className="col-md-12">
+        <SocialLinks links={['twitter', 'facebook', 'youtube']} />
+      </div>
+    </Container>
+  </div>
+);
 
 Contact.propTypes = {
   data: PropTypes.object,
