0a142189c1474439f927ddc97901a538db308c56
Myles Young <myles.young@mailchimp.com>
2020-11-07 21:16:11 -0500

    remove unused

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,4 @@
 import Box from './Box';
-import Button from './Button';
 import Card from './Card';
 import Center from './Center';
 import Divider from './Divider';
@@ -7,4 +6,4 @@ import FlexLayout from './FlexLayout';
 import Grid from './Grid';
 import StackLayout from './StackLayout';
 
-export { Box, Button, Card, Center, Divider, Grid, FlexLayout, StackLayout };
+export { Box, Card, Center, Divider, Grid, FlexLayout, StackLayout };
