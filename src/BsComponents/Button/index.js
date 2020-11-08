28fe377b4b60016fbd546a9850effd6c4fa9ef19
Myles Young <myles.young@mailchimp.com>
2020-11-07 21:57:37 -0500

    have button swap for links too

--- src/BsComponents/Button/index.js
+++ src/BsComponents/Button/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ children, className, type = 'button', onClick, ...rest }) => (
-  // eslint-disable-next-line react/button-has-type
-  <button type={type || 'button'} {...{ className, onClick, ...rest }}>
-    {children}
-  </button>
-);
+const Button = ({
+  children,
+  className,
+  type = 'button',
+  onClick,
+  href,
+  ...rest
+}) => {
+  if (href) {
+    return <a {...{ href, className, ...rest }}>{children}</a>;
+  }
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button type={type || 'button'} {...{ className, onClick, ...rest }}>
+      {children}
+    </button>
+  );
+};
 
 Button.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
   className: PropTypes.string,
   onClick: PropTypes.func,
   type: PropTypes.string,
+  href: PropTypes.string,
 };
 
 export default Button;
